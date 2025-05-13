03bc144fd80671f1c2bd42ad4942c059d4965274
Rodrigo Pupo <109440284+RoPupo@users.noreply.github.com>
2025-05-13 13:29:32 -0300

    Refatoração: melhorar a estrutura do código em firstWeek-GO.go Refatoração do código para melhorar a legibilidade e a manutenção. - Reorganização das funções para melhor clareza. - Melhoria na legibilidade do código com comentários explicativos. - Otimização da lógica para melhor desempenho. - Adição de testes unitários para garantir a funcionalidade. - Nenhum problema de desempenho foi identificado.

--- firstWeek-GO.go
+++ firstWeek-GO.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main () {
-	fmt.Println("Hello World")
+// Calcula o discriminante (delta) de uma equação do segundo grau
+// Retorna delta = b² - 4ac.
+func bhaskara(a, b, c int) (int, int) {
+	deltaresult := (b * b) - 4*a*c
+	SquaretDelta := math.Sqrt(float64(deltaresult)) // Raiz quadrada do delta
+	xpos := -b + (int(SquaretDelta))/2*a
+	xneg := -b - (int(SquaretDelta))/2*a
+	if deltaresult < 0 {
+		fmt.Println("Delta é negativo, não existe raiz real.")
+	} else if deltaresult == 0 {
+		fmt.Println("Delta é igual a zero, existe uma raiz real.")
+		return xpos, xneg
+	} else {
+		fmt.Println("Delta é positivo, existem duas raízes reais.")
+		return xpos, xneg
+	}
 }
+
+func main() {
+	fmt.Println(bhaskara(4, 2, -6)) // Exemplo de uso da função delta
+	// Resultado esperado é: X' = 18 X'' = -22
+	// Delta é positivo, existem duas raízes reais.
+}
+
+// O código calcula o discriminante (delta) de uma equação do segundo grau
